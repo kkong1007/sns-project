6d0a065233fe850788f827f12d799412c599e5ea
kkong1007 <vkslvkd1007@naver.com>
2021-05-07 20:21:35 +0900

    자기소개페이지

--- main/views.py
+++ main/views.py
@@ -5,3 +5,7 @@ def showmain(request):
     return render(request, 'main/mainpage.html')
 def show(request):
     return render(request, 'main/show.html')
+def bucket(request):
+    return render(request, 'main/bucket.html')
+def photo(request):
+    return render(request, 'main/photo.html')
--- sns_project/urls.py
+++ sns_project/urls.py
@@ -20,5 +20,7 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.showmain),
-    path('show/', views.show)
+    path('show/', views.show),
+    path('bucket/', views.bucket, name="bucket"),
+    path('photo/', views.photo, name="photo"),
 ]
