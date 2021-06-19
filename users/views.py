e5f46028907405efd9ede9532419ab678fbfb0ad
kkong1007 <vkslvkd1007@naver.com>
2021-06-19 15:29:37 +0900

    allauth와 user

---
+++ users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from main.models import Post
+from django.contrib.auth.models import User
+
+def mypage(request):
+    user=request.user
+    posts=Post.objects.filter(writer=user)
+    return render(request,'users/mypage.html', {'posts':posts})
+# Create your views here.
