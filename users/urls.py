a075b04a3728c7bd148bbe414688b39f30b6b52d
kkong1007 <vkslvkd1007@naver.com>
2021-07-16 14:23:38 +0900

    팔로우/팔로잉 구현

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 app_name="users"
 urlpatterns=[
-    path('mypage/', views.mypage, name="mypage"),
+    path('<int:id>/mypage', views.mypage, name="mypage"),
+    path('<int:id>/follow', views.follow, name="follow"),
+
 ]
